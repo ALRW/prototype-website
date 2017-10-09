292c0cdadb2b29bdcbfd3802af89b756ea1289d2
Andrew <awerner1@googlemail.com>
2017-10-09 13:34:05 +0000

    [AW] further test optimisation

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,24 +9,36 @@ describe 'Prototype App' do
     
     def app() Sinatra::Application end
     
-    it 'displays the homepage' do
+    before(:all) do
         get '/'
+    end
+    
+    def body
+        last_response.body
+    end
+    
+    it 'displays the homepage' do
         expect(last_response.status).to eq 200
     end
     
     it 'displays the company name' do
-        get '/'
-        expect(last_response.body).to include "Prototype Inc."
+        expect(body).to include "Prototype Inc."
     end
     
     it 'displays the correct content' do 
-        get '/'
-        expect(last_response.body).to include "Home of the World's Best Prototypes"
+        expect(body).to include "Home of the World's Best Prototypes"
     end
     
-    it 'displays the mail link with the correct address' do
-        get '/'
-        expect(last_response.body).to include "<li id=\"mail\"><a href=\"mailto:example@example.com\"><span class=\"glyphicon glyphicon-envelope\"></span></a></li>"
-    end
+    describe 'mailto link' do
         
+        it 'is right justified on the navigation bar' do
+            expect(body).to include "<ul class=\"nav navbar-nav navbar-right\">"
+        end
+        
+        it 'displays the mail link with the correct address' do
+            expect(body).to include "<li id=\"mail\"><a href=\"mailto:example@example.com\"><span class=\"glyphicon glyphicon-envelope\"></span></a></li>"
+        end
+        
+    end
+       
 end
